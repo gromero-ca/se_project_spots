68f737a12c2a9f684023693c4b72f0c3d096a945
Gabriel Romero <gabriel.romero@utec.edu.pe>
2025-08-22 17:42:06 -0700

    Enhance modal functionality with improved transitions and add profile form handling

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,17 @@ const editProfileBtnClose = editProfileModal.querySelector(".modal__close-btn");
 const newPostBtnOpen = profile.querySelector(".profile__add-btn");
 const editProfileBtnOpen = profile.querySelector(".profile__edit-btn");
 
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+
+const profileNameEl = profile.querySelector(".profile__name");
+const profileDescriptionEl = profile.querySelector(".profile__description");
+
 newPostBtnClose.addEventListener("click", function () {
   newPostModal.classList.remove("modal__is-opened");
 });
@@ -21,5 +32,16 @@ newPostBtnOpen.addEventListener("click", function () {
 });
 
 editProfileBtnOpen.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal__is-opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal__is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
